926d225f8cfac470d005b90665983056c996975c
Jakob <jakoseid@gmail.com>
2024-03-07 09:58:15 +0100

    Implemented Head pointer into struct

--- source/lexer/check_input.c
+++ source/lexer/check_input.c
@@ -6,7 +6,7 @@
 /*   By: jseidere <jseidere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 15:06:22 by jakob             #+#    #+#             */
-/*   Updated: 2024/03/06 16:57:14 by jseidere         ###   ########.fr       */
+/*   Updated: 2024/03/07 09:57:16 by jseidere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,6 @@ char	*skip_quotes(char *str)
 bool	check_input(t_shell *shell)
 {
 	t_token	*head;
-
 	if (shell->input[0] == '"')
 	{
 		shell->input = skip_quotes(shell->input);
@@ -101,8 +100,10 @@ bool	check_input(t_shell *shell)
 	norm_input(shell, token_count(shell) - 1 + count_chars(shell));
 	if (!shell->norm_input)
 		return (false);
+	//assign_token_types(shell);
 	head = assign_token_types(shell);
-	if (head)
-		print_tokens(&head);
+	shell->tokens = &head;
+	if (shell->tokens)
+		print_tokens(shell->tokens);
 	return(true);
 }
