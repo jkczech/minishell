b819310470d67a190c2fece75dd86e71f7d9f5f9
jkoupy <josef.koupy@seznam.cz>
2024-03-12 15:27:38 +0100

    get_commands

--- source/parser/parse.c
+++ source/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: jkoupy <jkoupy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 12:44:05 by jkoupy            #+#    #+#             */
-/*   Updated: 2024/03/12 13:39:14 by jkoupy           ###   ########.fr       */
+/*   Updated: 2024/03/12 15:27:22 by jkoupy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	parse(t_shell *shell)
 {
 	get_tokens(shell);
 	get_size(shell);
-    //get_commands(shell);
+    get_commands(shell);
 }
 
 //assigns token types to the tokens
@@ -46,4 +46,42 @@ void	get_size(t_shell *shell)
 		token = token->next;
 	}
 	shell->size = size + 1;
+}
+//creates command table
+void	get_commands(t_shell *shell)
+{
+	t_token	*token;
+	int		i;
+
+	token = *(shell->tokens);
+	if (!token)
+		return ;
+	shell->cmds = malloc(sizeof(t_cmd) * shell->size);
+	i = 0;
+	while (i < shell->size)
+	{
+		shell->cmds[i].args = NULL;
+		shell->cmds[i].path = NULL;
+		shell->cmds[i].found = false;
+		shell->cmds[i].input = -1;
+		shell->cmds[i].output = -1;
+		i++;
+	}
+	i = 0;
+	while (token)
+	{
+		if (token->token == PIPE)
+			i++;
+		else if (token->token == WORD)
+			shell->cmds[i].args = token->content;
+		else if (token->token == INPUT)
+			shell->cmds[i].input = open(token->content, O_RDONLY);
+		else if (token->token == OUTPUT)
+			shell->cmds[i].output = open(token->content, O_WRONLY | O_CREAT, 0644);
+		else if (token->token == HEREDOC)
+			shell->cmds[i].input = open(token->content, O_RDONLY);
+		else if (token->token == APPEND)
+			shell->cmds[i].output = open(token->content, O_WRONLY | O_APPEND | O_CREAT, 0644);
+		token = token->next;
+	}
 }
