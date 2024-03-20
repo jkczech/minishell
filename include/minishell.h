ba1be7a610297525d5d15d77e909db0e4d33a40f
jkoupy <josef.koupy@seznam.cz>
2024-03-20 11:23:07 +0100

    added find_commands()

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jkoupy <jkoupy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 12:04:06 by jkoupy            #+#    #+#             */
-/*   Updated: 2024/03/15 14:16:39 by jkoupy           ###   ########.fr       */
+/*   Updated: 2024/03/20 11:19:55 by jkoupy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,10 +89,8 @@ typedef struct s_shell
 	t_token			**tokens;
 	t_list			*history;
 	int				size;
-	int				infile; //
-	int				outfile; //
 	t_cmd			*cmds;
-	t_cmd			**s_cmds;
+	t_cmd			**s_cmds; //
 	int				**pipes;
 	char			**paths;
 	int				*child_pids;
@@ -108,9 +106,10 @@ typedef struct s_shell
 
 //builtins.c
 bool	init_shell(t_shell *shell, char **envp);
-bool	copy_envp(t_shell *shell, char **envp);
+//bool	copy_envp(t_shell *shell, char **envp);
 char	*get_path(t_shell *shell);
-void	handle_commands(t_shell *shell);
+//void	handle_commands(t_shell *shell);
+bool	is_builtin(t_shell *shell, int i);
 
 //builtins_utils.c
 void	free_double_pointer(char **ptr);
@@ -154,16 +153,17 @@ bool	free_array(char **array);
 
 //here_doc_bonus.c
 
-void	open_here_doc(t_shell *shell);
-void	here_doc(t_shell *shell);
+//void	open_here_doc(t_shell *shell);
+//void	here_doc(t_shell *shell);
 
 //parse.c
 
 bool	is_command(t_shell *shell, char *command, int i);
 void	find_command(t_shell *shell, int i);
 void	find_paths(t_shell *shell);
-void	open_files(t_shell *shell);
-bool	parse_input(t_shell *shell);
+void	find_commands(t_shell *shell);
+//void	open_files(t_shell *shell);
+//bool	parse_input(t_shell *shell);
 
 //pipex.c
 
@@ -220,7 +220,7 @@ int		open_heredoc(char *file);
 int		open_append(char *file);
 
 //parse.c
-void	parse(t_shell *shell);
+bool	parse(t_shell *shell);
 void	get_tokens(t_shell *shell);
 void	get_size(t_shell *shell);
 void	get_commands(t_shell *shell);
