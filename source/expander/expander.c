6aebd324cf8480aa4d7701d78f967aa6713e7623
Jakob Valentin Seiderer <jakoseid@gmail.com>
2024-04-05 16:57:04 +0200

    Marked failing part

--- source/expander/expander.c
+++ source/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: jseidere <jseidere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 10:54:05 by jkoupy            #+#    #+#             */
-/*   Updated: 2024/04/05 16:53:01 by jseidere         ###   ########.fr       */
+/*   Updated: 2024/04/05 16:56:59 by jseidere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ char	*get_env_value(t_shell *shell, char *str)
 		printf("str: %s\n", str);
 		printf("i: %d\n", i);
 		// Find solution if expanding variable is within a string
+		//Here
 		if (ft_strncmp(var, str, i) == 0)
 		{
 			printf("SUCCESS\n");
