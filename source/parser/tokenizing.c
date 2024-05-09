ace18adc03cfb664f7721e499f2b254ef813db61
Jakob Valentin Seiderer <jakoseid@gmail.com>
2024-05-09 16:49:34 +0200

    Norming

--- source/parser/tokenizing.c
+++ source/parser/tokenizing.c
@@ -6,7 +6,7 @@
 /*   By: jseidere <jseidere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 14:42:17 by jseidere          #+#    #+#             */
-/*   Updated: 2024/05/09 12:26:02 by jseidere         ###   ########.fr       */
+/*   Updated: 2024/05/09 16:48:48 by jseidere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,12 +47,8 @@ void	process_token(char *str, int *i, int token_type, t_token **head)
 	while (str[*i] && (!is_delimiter(str[*i], DELIMITER) || quote.q_closed))
 	{
 		if (is_quote(str[*i]))
-		{
 			quote.q_closed = !quote.q_closed;
-			//process_quoted_token(str, i, token_content, &j);
-		}
 		token_content[j++] = str[(*i)++];
-		//printf("Str[%d]: %c\n", *i, str[*i]);
 	}
 	token_content[j++] = '\0';
 	new_token = create_token(token_content, token_type);
